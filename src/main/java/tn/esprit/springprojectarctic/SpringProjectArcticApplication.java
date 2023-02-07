6aada25e7fdb5620499d5e7c277dcc01b222991e
zackdarklord <zakaria.chelbi@esprit.tn>
2023-02-07 14:14:17 +0100

    first commit

---
+++ src/main/java/tn/esprit/springprojectarctic/SpringProjectArcticApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.springprojectarctic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectArcticApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProjectArcticApplication.class, args);
+    }
+
+}
